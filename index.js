44749dd5d410fd11e53c101a73718111615dbc21
JeisonAlexis <jeyconalex123@gmail.com>
2025-08-24 17:27:19 -0500

    Pregunta 16

--- index.js
+++ index.js
@@ -905,6 +905,38 @@ app.get("/prueba_fase1", async (req, res) => {
   }
 });
 
+app.get("/prueba_fase2", async (req, res) => {
+  try {
+    const { data: html } = await axios.get(
+      "https://portal.senasofiaplus.edu.co/index.php/ayudas/preguntas-frecuentes",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0 (compatible; Botpress/1.0)",
+        },
+      }
+    );
+
+    const $ = cheerio.load(html);
+
+    const titulo = $(
+      'div.titulopregunta:contains("¿De qué se compone la prueba Fase II del proceso y que busca?")'
+    );
+
+    if (titulo.length === 0) {
+      return res.status(404).json({ error: "Pregunta no encontrada" });
+    }
+
+    const respuesta = titulo.next(".respuesta").html();
+
+    return res.json({
+      pregunta: titulo.text().trim(),
+      respuesta: respuesta.trim(),
+    });
+  } catch (error) {
+    console.error("Error al obtener los datos:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puertoo ${port}`);
