1dc787b47c75feab6e900de3daaa4e67014c9ec5
JeisonAlexis <jeyconalex123@gmail.com>
2025-09-28 18:55:07 -0500

    consultar_prueba

--- index.js
+++ index.js
@@ -1479,6 +1479,37 @@ app.get("/cambio_contrasena", async (req, res) => {
   }
 });
 
+app.get("/consultar_resultados_pruebas", async (req, res) => {
+  try {
+    const url = "https://portal.senasofiaplus.edu.co/index.php/ayudas";
+    const baseUrl = "https://portal.senasofiaplus.edu.co"; 
+
+    const { data } = await axios.get(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (compatible; Botpress/1.0)",
+      },
+    });
+
+    const $ = cheerio.load(data);
+
+    let pasos = [];
+    $("div.imagens img").each((i, el) => {
+      const src = $(el).attr("src");
+      if (src && src.includes("9_consultar_resultados_pruebas")) {
+        const fullUrl = src.startsWith("http") ? src : baseUrl + src;
+        pasos.push(fullUrl);
+      }
+    });
+
+    res.json({
+      pasos,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener las imágenes" });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
